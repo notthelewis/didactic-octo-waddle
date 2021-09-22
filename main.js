cfff227e4c1c5af453a8bf3fa4f7eb5af8c25b63
Lewis Farnworth <lewfar99@gmail.com>
2021-09-22 23:09:14 +0100

    Slightly changed error message format.

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const
 function* countAllFiles() {
     for (let i = 0; i < args.length; i++){
         fs.access(args[i], e => {
-            !e ? countLinesInFile(args[i]) : console.log(`${args[i]} does not exist.`);
+            !e ? countLinesInFile(args[i]) : console.log(`${args[i]} does not exist or is inaccesible:\n${e}.`);
         });
         yield;
     }
