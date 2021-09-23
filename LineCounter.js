6155e851204e48bf0920dea06d9998765d1f6a4c
Lewis Farnworth <lewfar99@gmail.com>
2021-09-23 21:42:22 +0100

    Renamed LineTally

--- LineCounter.js
+++ LineCounter.js
@@ -1,22 +1,34 @@
 const { createReadStream } = require('fs');
 const { createInterface } = require('readline');
 const { InterestingRegex } = require('./InterestingRegex');
+const { TallyObject } = require('./Tally');
 
-exports.LineTally = class LineTally {
+exports.LineCounter = class LineCounter {
     constructor(fileName) {
         this.fileName = fileName;
         this.interestingRegex = new InterestingRegex();
+        this.tally = new TallyObject();
 
         this.lineReaderObject = createInterface({
             input: createReadStream(this.fileName),
             console: false
         });
-    }
+    };
 
     async startTally () {
         for await (const line of this.lineReaderObject) {
             this.matches = this.interestingRegex.checkMatch(line);
-            console.log(line, this.matches);
+            console.log(this.matches);
+
+        //     if (this.matches.length == 0) {
+        //         this.tally.incrementCodeLine();
+        //     } else {
+        //         switch (this.matches) {
+        //             case ['']: 
+
+        //         }
+        //     }
         }
-    }
-}
+        // console.log(this.tally.counter);
+    };
+};
