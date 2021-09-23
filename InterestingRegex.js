00409dd9c598ed4d2a67185748ebecfc7ff437fe
Lewis Farnworth <lewfar99@gmail.com>
2021-09-23 01:11:57 +0100

    Class containing all interesting regexes. Each regex is used as a key for a hashtable. The value for each key is a friendly name for the regex. There's two methods exposed by this class; a checkMatchGenerator function, which uses a generator function to iterate the hash table, yielding each time it finds a match.

---
+++ InterestingRegex.js
@@ -0,0 +1,32 @@
+class InterestingRegex {
+    constructor() {
+        this.regexList = new Map();
+        this.regexList.set(/\/\*[\s\S]*?/, 'comment_ML_StartComment');
+        this.regexList.set(/[\s\S]*?\*\//, 'comment_ML_EndComment');
+        this.regexList.set(/\/\/.*/, 'comment_SL_StartOfLine');
+        this.regexList.set(/[^\/]\/\/.*/, 'comment_SL_EndOfLine');
+        this.regexList.set(/(^(\r\n|\n|\r)$)|(^(\r\n|\n|\r))|^\s*$/gm, 'blankLine');
+    }
+}
+
+InterestingRegex.prototype.checkMatchGenerator = function* (line) {
+    let foundRegex;
+
+    for (regex of this.regexList) {
+        if (regex[0].test(line)) {
+            foundRegex = true;
+            yield regex[1];
+        }
+    }
+
+    if (foundRegex == 'undefined') {
+        yield 'codeLine';
+    }
+
+    return;
+}
+
+InterestingRegex.prototype.checkMatch = function (line) {
+    return [...this.checkMatchGenerator(line)];
+}
+exports.InterestingRegex = InterestingRegex;
