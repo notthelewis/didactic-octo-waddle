5eb22b47c6dc513a63da6193de9466b16a3af787
Lewis Farnworth <lewfar99@gmail.com>
2021-09-24 00:37:29 +0100

    This file was deleted for some reason... adding back in.

---
+++ LineCounter.js
@@ -0,0 +1,33 @@
+/* 
+    LineCounter is an implementation of the Facade design pattern. 
+    It provides a unified interface to a set of subsystems (lineReader, PatternMatcher, Tallier, Tally)
+*/
+
+const { createReadStream } = require('fs');
+const { createInterface } = require('readline');
+const { PatternMatcher } = require('./PatternMatcher');
+const { Tallier } = require('./Tallier');
+
+exports.LineCounter = class LineCounter {
+    constructor(fileName) {
+        this.fileName = fileName;
+        
+        this.lineReaderObject = createInterface({
+            input: createReadStream(this.fileName),
+            console: false
+        });
+        
+        this.PatternMatcher = new PatternMatcher();
+        this.tallier = new Tallier()
+    };
+
+    async startTally () {
+        for await (const line of this.lineReaderObject) {
+            this.matches = this.PatternMatcher.checkMatch(line);
+            this.tallier.tallyLine(this.matches);
+        }
+
+        this.tallier.tally.getTotalLines();
+        return this.tallier.tally.counter;
+    };
+};
