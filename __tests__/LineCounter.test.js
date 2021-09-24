dad6a1236133c9e2b241be23621f1d424fb56767
Lewis Farnworth <lewfar99@gmail.com>
2021-09-24 14:56:45 +0100

    Added an integration test on the Facade class, to ensure that all the code works together to return the right result.

---
+++ __tests__/LineCounter.test.js
@@ -0,0 +1,17 @@
+const { LineCounter } = require("../LineCounter");
+
+integrationTest = () => {
+    test("Check that passing the example source code file (source.c) into LineCounter returns the expected results", async () => {
+        let source_c_counter = new LineCounter('source.c');
+        expect(await source_c_counter.startTally()).toStrictEqual({
+            codeLines: 5,
+            blankLines: 1,
+            MLCommentBlocks: 2,
+            MLCommentLines: 5,
+            SLComments: 1,
+            totalLines: 12
+        });
+    });
+}
+
+integrationTest();
