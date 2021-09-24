2962af74fb7fbe7860317a8ce51d4357d2ed6642
Lewis Farnworth <lewfar99@gmail.com>
2021-09-24 23:12:30 +0100

    Changed the format of some of the comments, so that they're more inclusive of different comment styles.

--- source.c
+++ source.c
@@ -5,8 +5,10 @@
     Multi
     Line
  */
+// Single line which starts at the very start of a line.
+//Single line which starts at the very start of a line, but also has no whitespace after the 
 int main(int argc, char const *argv[])
 {
-    // Code here
+    // Single line comment which begins with whitespace
     return 0;
 }
