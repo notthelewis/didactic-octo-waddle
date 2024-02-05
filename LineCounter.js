c4db6039b2ad3cf811c9e80047f1295d69e9244e
wsl-11-2023 <lewfar99@gmail.com>
2024-02-05 22:55:04 +0000

    style(review): Update code style
    Remove some stupid design choices that I made in this toy project a
    while ago, because I've decided to rigorously dig through old code
    projects and update them, as I have nothing better to do with my time
    apparently.

--- LineCounter.js
+++ LineCounter.js
@@ -3,38 +3,32 @@
     It provides a unified interface to a set of subsystems (lineReader, PatternMatcher, Tallier, Tally)
 */
 
-const
-    { createReadStream } = require('fs'),
-    { createInterface } = require('readline'),
-    { PatternMatcher } = require('./PatternMatcher'),
-    { Tallier } = require('./Tallier')
-;
+const { createReadStream } = require("fs");
+const { createInterface } = require("readline");
+const { PatternMatcher } = require("./PatternMatcher");
+const { Tallier } = require("./Tallier");
 
 exports.LineCounter = class LineCounter {
-    constructor(fileName) {
-        this.fileName = fileName;
-        this.lineReaderObject = createInterface({
-            input: createReadStream(this.fileName),
-            console: false
-        });
-        
-        this.PatternMatcher = new PatternMatcher();
-        this.tallier = new Tallier()
-    };
+  constructor(fileName) {
+    this.PatternMatcher = new PatternMatcher();
+    this.tallier = new Tallier();
+    this.fileName = fileName;
+    this.lineReaderObject = createInterface({
+      input: createReadStream(this.fileName),
+      console: false,
+    });
+  }
 
-    async startTally () {
-        /* For ... of .. to iterate over each line independently. */
-        for await (const line of this.lineReaderObject) {
-            /* Get an array of regexes which match this current line*/
-            this.matches = this.PatternMatcher.checkMatch(line);
-            /* Pass this array to the Tallier, so that it can count based on each condition. (single-line comment, multi-line comment, blank-line or none) */
-            this.tallier.tallyLine(this.matches);
-        }
+  async startTally() {
+    for await (const line of this.lineReaderObject) {
+      this.matches = this.PatternMatcher.checkMatch(line);
+      this.tallier.tallyLine(this.matches);
+    }
 
-        /* Outside the loop, add together all of the matched values */
-        this.tallier.tally.getTotalLines();
-        this.lineReaderObject.close();
-        /* Return the counter object */
-        return this.tallier.tally.counter;
-    };
+    // Outside the loop, add together all of the matched values
+    this.tallier.tally.getTotalLines();
+    this.lineReaderObject.close();
+
+    return this.tallier.tally.counter;
+  }
 };
